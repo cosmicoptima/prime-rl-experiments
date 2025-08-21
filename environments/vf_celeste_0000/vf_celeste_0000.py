8f94b925a74c0f2c52a0d60bc44f51dd2185ddd2
celeste <parafactual@gmail.com>
2025-08-21 04:33:29 -0700

    Small fixes to first environment

--- environments/vf_celeste_0000/vf_celeste_0000.py
+++ environments/vf_celeste_0000/vf_celeste_0000.py
@@ -1,11 +1,11 @@
 from datasets import load_dataset
 import verifiers as vf
 
-from string import ascii_uppercase
+from string import ascii_letters, ascii_uppercase
 
 
 def load_environment(**kwargs) -> vf.Environment:
-    dataset = load_dataset("tatsu-lab/alpaca")
+    dataset = load_dataset("tatsu-lab/alpaca", split="train")
     dataset = dataset.map(lambda x: {
         "question": f"{x['instruction']}{': ' if x['input'] else ''}{x['input']}",
         "answer": {},
@@ -13,9 +13,12 @@ def load_environment(**kwargs) -> vf.Environment:
     })
 
     def reward(completion, answer, **kwargs):
-        return len([c for c in completion if c in ascii_uppercase]) / len(completion)
+        n_letters = len([c for c in answer if c in ascii_letters])
+        if n_letters == 0:
+            return 0.0
+        return len([c for c in completion if c in ascii_uppercase]) / n_letters
 
-    return vf.Environment(
+    return vf.SingleTurnEnv(
         dataset=dataset,
         rubric=vf.Rubric(funcs=[reward], weights=[1.0]),
     )
