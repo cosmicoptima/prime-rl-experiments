60e0f63a29b693be19a513b2398481e1194753e2
celeste <parafactual@gmail.com>
2025-08-27 18:48:35 -0700

    reduce micro bs and add flash attn thing to setup sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
 uv sync && uv sync --all-extras
+uv pip uninstall flash_attn
+uv pip install flash_attn --no-build-isolation --no-cache-dir
 
 # Prime-RL setup for runpod persistence
 
